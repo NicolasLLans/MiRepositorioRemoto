e4db14687b097565a8f831848a3e9b536200ea7b
NicolasLLans <nicolaslautaro22@gmail.com>
2022-04-08 19:56:56 -0300

    Front end en angular de mi profolio

---
+++ Front-End-Angular/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './component/nav-bar/nav-bar.component';
+import { EncabezadoComponent } from './component/encabezado/encabezado.component';
+import { AcercaDeComponent } from './component/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './component/experiencia/experiencia.component';
+import { EducacionComponent } from './component/educacion/educacion.component';
+import { SkillsComponent } from './component/skills/skills.component';
+import { ProyectosComponent } from './component/proyectos/proyectos.component';
+import {HttpClientModule} from '@angular/common/http'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    EncabezadoComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    SkillsComponent,
+    ProyectosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
