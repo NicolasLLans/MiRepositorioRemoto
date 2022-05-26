33600d442d582def3541f9fd4b6060d169a796a9
NicolasLLans <nicolaslautaro22@gmail.com>
2022-05-25 23:43:10 -0300

    Seccion de Proyectos dinamica, con botones de edicion con crud completo

--- FEAngular/FrontEndAngular/src/app/componentes/proyectos/proyectos.component.ts
+++ FEAngular/FrontEndAngular/src/app/componentes/proyectos/proyectos.component.ts
@@ -1,5 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Proyectos } from 'src/app/models/proyectos';
 import { ProyectosService } from 'src/app/servicios/proyectos.service';
 
@@ -11,6 +12,8 @@ import { ProyectosService } from 'src/app/servicios/proyectos.service';
 export class ProyectosComponent implements OnInit {
 
   public proyectos:Proyectos[]=[];
+  public editarProyectos:Proyectos | undefined;
+  public deleteProyectos:Proyectos | undefined;
   
   constructor(private proyectosService:ProyectosService) { }
 
@@ -24,8 +27,68 @@ export class ProyectosComponent implements OnInit {
       this.proyectos=Response;
       },
       error:(error:HttpErrorResponse)=>{
-        alert(error.message)
+        alert(error.message) 
       }
     });
   }
+  public onOpenModal(mode:String, proyectos?: Proyectos):void{
+    const container=document.getElementById('main-container');
+    const button=document.createElement('button');
+    button.style.display='none';
+    button.setAttribute('data-toggle','modal');
+    if(mode==='add'){
+      button.setAttribute('data-target','#addProyectosModal');
+    }else if(mode==='delete'){
+      this.deleteProyectos=proyectos;
+      button.setAttribute('data-target','#deleteProyectosModal');
+    }else if(mode==='edit'){
+      this.editarProyectos=proyectos;
+      button.setAttribute('data-target','#editProyectosModal');
+    }
+    container?.appendChild(button); 
+    button.click();
+    console.log("llama a la funcion");
+  }
+
+  public onAddProyectos(addForm: NgForm):void{
+    document.getElementById('add-proyectos-form')?.click();
+    this.proyectosService.addProyectos(addForm.value).subscribe({
+      next: (response:Proyectos) =>{
+        console.log(response);
+        this.obtenerProyectos();
+        addForm.reset();
+      },
+      error:(error:HttpErrorResponse)=>{
+        alert(error.message)
+        addForm.reset();
+      }
+    })
+  }
+
+  public onUpdateProyectos(proyectos: Proyectos){
+    this.editarProyectos=proyectos;
+    document.getElementById('add-proyectos-form')?.click();
+    this.proyectosService.editarProyectos(proyectos).subscribe({
+      next: (response:Proyectos) =>{
+        console.log(response);
+        this.obtenerProyectos();
+      },
+      error:(error:HttpErrorResponse)=>{
+        alert(error.message)
+      }
+    })
+  }
+
+  public onDeleteProyectos(id:number):void{
+    this.proyectosService.borrarProyectos(id).subscribe({
+      next: (response:void) =>{
+        console.log(response);
+        this.obtenerProyectos();
+      },
+      error:(error:HttpErrorResponse)=>{
+        alert(error.message)
+      }
+    })
+  }
+
 }
--- FEAngular/FrontEndAngular/src/app/servicios/proyectos.service.ts
+++ FEAngular/FrontEndAngular/src/app/servicios/proyectos.service.ts
@@ -16,4 +16,14 @@ export class ProyectosService {
   public obtenerProyectos():Observable<Proyectos[]>{
     return this.http.get<Proyectos[]>(`${this.apiServerUrl}/Proyectos/all`);
   }
+
+  public addProyectos(proyectos: Proyectos):Observable<Proyectos>{
+    return this.http.post<Proyectos>(`${this.apiServerUrl}/Proyectos/add`,proyectos);
+  }
+  public editarProyectos(proyectos: Proyectos):Observable<Proyectos>{
+    return this.http.put<Proyectos>(`${this.apiServerUrl}/Proyectos/update`,proyectos);
+  }
+  public borrarProyectos(proyectosId: number):Observable<void>{
+    return this.http.delete<void>(`${this.apiServerUrl}/Proyectos/delete/${proyectosId}`);
+  }
 }
